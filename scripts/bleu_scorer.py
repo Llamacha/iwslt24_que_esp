0086ba1d0a970c5bdbc96c0db2f78b71b2381a28
unknown <rjzevallos.salazar@gmail.com>
2023-04-05 16:13:15 +0200

    update

--- scripts/bleu_scorer.py
+++ scripts/bleu_scorer.py
@@ -4,44 +4,73 @@ import argparse
 from sacrebleu.metrics import BLEU
 
 
-def parse_input():
-    parser = argparse.ArgumentParser("Evaluation of Speech Translation")
-    parser.add_argument("--ref", help="root of ref-text, txt file")
-    parser.add_argument("--hyp", help="root of hyp-text, txt file")
+def parse_args():
+    """
+    Parsea los argumentos de línea de comandos.
+    :return: Los argumentos parseados.
+    """
+    parser = argparse.ArgumentParser(description="Evaluation of Speech Translation")
+    parser.add_argument("--ref", required=True, help="path of reference text file")
+    parser.add_argument("--hyp", required=True, help="path of hypothesis text file")
     args = parser.parse_args()
     return args 
 
 
-def read_hyps(root):
-    with open(root, 'r', encoding='utf8') as f:
-        hyps = [x.strip() for x in f]
-    return hyps
+def read_hypotheses_file(path):
+    """
+    Lee el archivo de hipótesis y lo retorna como una lista de strings.
+    :param path: La ruta del archivo de hipótesis.
+    :return: Una lista de strings, cada uno siendo una hipótesis.
+    """
+    with open(path, 'r', encoding='utf-8') as f:
+        hypotheses = [line.strip() for line in f]
+    return hypotheses
 
 
-def read_refs(root):
-    refs = []
-    with open(root, 'r', encoding='utf8') as f:
-        refss = [x.strip() for x in f]
-        refs.append(refss)
-    return refs
+def read_references_file(path):
+    """
+    Lee el archivo de referencias y lo retorna como una lista de listas de strings.
+    :param path: La ruta del archivo de referencias.
+    :return: Una lista de listas de strings, cada lista interna siendo las referencias para una hipótesis.
+    """
+    references = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            references.append([line.strip()])
+    return references
 
 
-def result_blue(hyps,refs):
+def calculate_bleu(hypotheses, references):
+    """
+    Calcula el puntaje BLEU para una lista de hipótesis y sus referencias.
+    :param hypotheses: La lista de hipótesis.
+    :param references: La lista de referencias.
+    :return: El puntaje BLEU.
+    """
     bleu = BLEU()
-    return bleu.corpus_score(hyps,refs)
+    return bleu.corpus_score(hypotheses, references)
 
 
-def output_file(result):
-    with open(root, 'w', encoding='utf8') as f:
-        f.write(result)
+def write_output_file(result, path):
+    """
+    Escribe el puntaje BLEU en un archivo de texto.
+    :param result: El puntaje BLEU a escribir.
+    :param path: La ruta del archivo de salida.
+    """
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(str(result))
 
 
 def main():
-    args = parse_input() 
-    hyps = read_hyps(args.hyp)
-    refs = read_refs(args.ref)
-    result = result_blue(hyps,refs)
-    print(result)
+    """
+    Función principal que lee los archivos de hipótesis y referencias, calcula el puntaje BLEU y lo imprime.
+    """
+    args = parse_args()
+    hypotheses = read_hypotheses_file(args.hyp)
+    references = read_references_file(args.ref)
+    bleu_score = calculate_bleu(hypotheses, references)
+    print(f"BLEU score: {bleu_score}")
+    write_output_file(bleu_score, "output.txt")
 
 
 if __name__ == "__main__":
--- scripts/wer_cer.py
+++ scripts/wer_cer.py
@@ -3,41 +3,50 @@
 from evaluate import load
 import argparse
 
-hyp = ["hello world", "good night moon"]
-ref = ["hello world", "good night moon"]
-
 
 def parse_input():
+    """Parse command-line arguments."""
     parser = argparse.ArgumentParser("Evaluation of Speech Translation")
-    parser.add_argument("--ref", help="root of ref-text, txt file")
-    parser.add_argument("--hyp", help="root of hyp-text, txt file")
-    args = parser.parse_args()
-    return args
+    parser.add_argument("--ref", help="path to reference text file", required=True)
+    parser.add_argument("--hyp", help="path to hypothesis text file", required=True)
+    return parser.parse_args()
 
 
-def read_file(root):
-    with open(root, 'r', encoding='utf8') as f:
-        list_txt = [x.strip() for x in f]
-    return list_txt
+def read_file(path):
+    """Read text file and return list of strings, one for each line."""
+    with open(path, 'r', encoding='utf8') as f:
+        lines = [line.strip() for line in f]
+    return lines
 
 
-def get_evaluation_result(hyp,ref,metric):
-    if metric == "cer":
-        eva = load("cer")
-    else:
-        eva = load("wer")
-    cer_score = eva.compute(predictions=hyp, references=ref)
-    return cer_score
+def compute_error_rate(hypotheses, references, metric):
+    """
+    Compute Character Error Rate (CER) or Word Error Rate (WER) between
+    a list of hypotheses and references.
+
+    Args:
+        hypotheses (List[str]): List of hypotheses, where each hypothesis is a string.
+        references (List[str]): List of references, where each reference is a string.
+        metric (str): Evaluation metric to use, either 'cer' for CER or 'wer' for WER.
+
+    Returns:
+        float: Error rate score between 0 and 1, where lower scores are better.
+    """
+    evaluator = load(metric)
+    return evaluator.compute(predictions=hypotheses, references=references)
 
 
 def main():
     args = parse_input()
-    hyp = read_file(args.hyp)
-    ref = read_file(args.ref)
-    cer_result = get_evaluation_result(hyp,ref,"cer")
-    print("CER on original hypothesis Error_Rate= "+str(cer_result))
-    wer_result = get_evaluation_result(hyp,ref,"wer")
-    print("WER on original hypothesis Error_Rate= "+str(wer_result))
+    hypotheses = read_file(args.hyp)
+    references = read_file(args.ref)
+
+    # Compute CER and WER and print results
+    cer_score = compute_error_rate(hypotheses, references, "cer")
+    print(f"CER on hypotheses: {cer_score:.4f}")
+
+    wer_score = compute_error_rate(hypotheses, references, "wer")
+    print(f"WER on hypotheses: {wer_score:.4f}")
 
 
 if __name__ == "__main__":
